1a9da9906a95386e38f8f23b23dc766fce857bc3
Savio Lima <saviiolima@gmail.com>
2024-11-27 10:41:02 -0300

    atualizando o footer e queroDoar

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import Header from './components/Header/header'
 import Footer from './components/Footer/footer'
-import './styles/global.scss'
+import './Styles/global.scss'
 
 
 export default function App() {
--- src/Pages/QueroDoar/QueroDoar.jsx
+++ src/Pages/QueroDoar/QueroDoar.jsx
@@ -2,25 +2,27 @@ import S from "./querodoar.module.scss";
 import bookdoados from "../../assets/img/bookdoados.png";
 
 export default function QueroDoar() {
-    return (
-        <main>
+  return (
+    <main>
+      <section className={S.boxPrincipal}>
         <section className={S.boxDoados}>
-          <p>
-            Por favor, preencha o formulário com suas informações e as informações
-            do Livro
-          </p>
-        </section>
-        <div className={S.boxLivros}>
-            <div className={S.boxForms}>
-              <img src={bookdoados} alt="Livro" />
-              <h2>Informações do Livro</h2>
+          <h2>
+            Por favor, preencha o formulário com suas informações e as
+            informações do Livro
+          </h2>
+          <form action="">
+            <div>
+              <img src={bookdoados} alt="icone de um livro" />
+              <h3>Informações do Livro </h3>
             </div>
-            <input type="text" placeholder="Titulo" />
+            <input type="text" placeholder="Título" />
             <input type="text" placeholder="Categoria" />
             <input type="text" placeholder="Autor" />
             <input type="text" placeholder="Link de Imagem" />
-            <button className={S.boxButton}><p>Doar</p></button>
-        </div>
-      </main>
-    )
+            <input type="submit" value="Doar" />
+          </form>
+        </section>
+      </section>
+    </main>
+  );
 }
