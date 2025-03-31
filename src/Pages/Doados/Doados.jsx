f35f9dee3d439f265fb592e71e2cffad3295bb21
Savio Lima <saviiolima@gmail.com>
2025-03-31 15:25:03 -0300

    finalizando a api

--- src/Pages/Doados/Doados.jsx
+++ src/Pages/Doados/Doados.jsx
@@ -1,7 +1,22 @@
 import S from "./doados.module.scss";
 import booksdoados from "../../assets/img/booksdoados.png";
+import axios from "axios";
+import { useEffect, useState } from "react";
 
 export default function Doados() {
+  const [livros, setLivros] = useState([]);
+
+  const getLivros = async () => {
+    const response = await axios.get(
+      "https://api-livros-c1pr.onrender.com/livros"
+    );
+    setLivros(response.data);
+  };
+
+  useEffect(() => {
+    getLivros();
+  }, []);
+
   return (
     <main>
       <section className={S.boxDoados}>
@@ -13,12 +28,14 @@ export default function Doados() {
             <p>Susanne Andrade</p>
             <p>Ficção</p>
           </article>
-          <article>
-            <img src={booksdoados} alt="livro o protagonista" />
-            <h3>O protagonista</h3>
-            <p>Susanne Andrade</p>
-            <p>Ficção</p>
-          </article>
+          {livros.map((item) => (
+            <article key={item.id}>
+              <img src={item.imagem_url} alt="" />
+              <h3>{item.titulo}</h3>
+              <p>{item.categoria}</p>
+              <p>{item.autor}</p>
+            </article>
+          ))}
         </section>
       </section>
     </main>
--- src/Pages/QueroDoar/QueroDoar.jsx
+++ src/Pages/QueroDoar/QueroDoar.jsx
@@ -1,7 +1,52 @@
 import S from "./querodoar.module.scss";
 import bookdoar from "../../assets/img/bookdoar.png";
 
+import axios from "axios";
+import { useState } from "react";
+
 export default function QueroDoar() {
+  const [titulo, setTitulo] = useState("");
+  const [categoria, setCategoria] = useState("");
+  const [autor, setAutor] = useState("");
+  const [imagem_url, setImagem] = useState("");
+
+  const enviarDados = async () => {
+    const urlApi = "https://api-livros-c1pr.onrender.com/doar";
+
+    const dadosEnviar = {
+      titulo,
+      categoria,
+      autor,
+      imagem_url,
+    };
+
+    await axios.post(urlApi, dadosEnviar);
+
+    alert("Livro enviado!");
+
+    setTitulo("");
+    setCategoria("");
+    setAutor("");
+    setImagem("");
+  };
+
+  const capturaTitulo = (e) => {
+    setTitulo(e.target.value);
+    console.log(e.target.value);
+  };
+
+  const capturaCategoria = (e) => {
+    setCategoria(e.target.value);
+  };
+
+  const capturaAutor = (e) => {
+    setAutor(e.target.value);
+  };
+
+  const capturaImagem = (e) => {
+    setImagem(e.target.value);
+  };
+
   return (
     <main>
       <section className={S.boxPrincipal}>
@@ -10,16 +55,36 @@ export default function QueroDoar() {
             Por favor, preencha o formulário com suas informações e as
             informações do Livro
           </h2>
-          <form action="">
+          <form onSubmit={(e) => e.preventDefault()}>
             <div>
               <img src={bookdoar} alt="icone de um livro" />
               <h3>Informações do Livro </h3>
             </div>
-            <input type="text" placeholder="Título" />
-            <input type="text" placeholder="Categoria" />
-            <input type="text" placeholder="Autor" />
-            <input type="text" placeholder="Link de Imagem" />
-            <input type="submit" value="Doar" />
+            <input
+              type="text"
+              placeholder="Título"
+              onChange={capturaTitulo}
+              value={titulo}
+            />
+            <input
+              type="text"
+              placeholder="Categoria"
+              onChange={capturaCategoria}
+              value={categoria}
+            />
+            <input
+              type="text"
+              placeholder="Autor"
+              onChange={capturaAutor}
+              value={autor}
+            />
+            <input
+              type="text"
+              placeholder="Link de Imagem"
+              onChange={capturaImagem}
+              value={imagem_url}
+            />
+            <input onClick={enviarDados} type="submit" value="Doar" />
           </form>
         </section>
       </section>
